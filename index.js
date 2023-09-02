4ceb6d5654e862bac2c5f3e3bdbbb7f2fe9b50cd
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-09-02 11:21:22 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const removeMain = document.getElementById("main").remove();
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = "YOUR-NAME is the champion";
+newHeader.id = "victory";
+document.body.append(newHeader);
+
